c3c9750a57a5a93d67001e3954e7db89246b1af1
John Cotterell <Cotterzz@users.noreply.github.com>
2019-03-14 01:15:27 +0000

    Create cheatsheet.js

---
+++ cheatsheet.js
@@ -0,0 +1,16 @@
+// Javascript Cheatsheet By John Cotterell johnmdcotterell@gmail.com www.cotterzz.com  github.com/cotterzz/
+
+// Part 0 - COMMENTS. This is a single line comment
+
+// Comments are ignored by the parser and are for humans to read
+
+/* This is a
+        multi line
+               comment */
+
+// Variable assignment
+var x;  // this declares variable x, without assigning it a value.
+x = 42; // this assigns a value to x, without declaring it first.
+var x = 42; // this declares variable x and assigns it a numerical value, in one line.
+var x, y, z; // this declares several variables in one go.
+var x=42, y=53, z=67; // this declares several variables and assigns them values.
